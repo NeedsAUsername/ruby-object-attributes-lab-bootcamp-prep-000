eb31aad9be26d675569bd15cd6f4aae50e87fbae
Kevin Chi <kevinchi118@gmail.com>
2018-06-09 16:53:46 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,16 +1,14 @@
 class Person
-  def initialize(name = "beyonce")
+  def initialize(name, job)
     @name = name
-  end
-  def name 
-    @name 
+    @job = job
+  end 
+  def name
+    @name
   end 
   def name=(new_name)
     @name = new_name
-  end 
-  def initialize(job)
-    @job = job
-  end 
+  end
   def job
     @job
   end 
